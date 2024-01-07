26e117d02540db01fdeede431bcdbbaa35092c0e
tenfyzhong <tenfy@tenfy.cn>
2024-01-07 15:59:35 +0800

    go test abbr

--- conf.d/golang.fish
+++ conf.d/golang.fish
@@ -2,7 +2,7 @@ abbr -a lgob CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
 abbr -a wgob CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build
 abbr -a tgob go build -gcflags "-N -l" -ldflags "-w"
 abbr -a gob go build
-abbr -a got go test -gcflags=all=-l
+abbr -a got go test -gcflags=all=-l -v -cover
 abbr -a gog go get -u
 abbr -a gom go mod
 abbr -a goc go clean
