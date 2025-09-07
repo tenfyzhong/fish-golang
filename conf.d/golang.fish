c37740ea809b9a2d59738d0c2b2ddccf3b6b69ba
tenfyzhong <tenfy@tenfy.cn>
2025-09-07 18:14:55 +0800

    feat(golang): Enhance Go aliases for testing and building
    - Refactor `tgob` alias to correctly pass `-w` to `ldflags`, ensuring proper linker behavior.
    - Add `-tags=intest` to `got` alias to enable selective execution of tests based on build tags.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- conf.d/golang.fish
+++ conf.d/golang.fish
@@ -1,8 +1,8 @@
 abbr -a lgob CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
 abbr -a wgob CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build
-abbr -a tgob go build -gcflags "-N -l" -ldflags "-w"
+abbr -a tgob go build -gcflags "-N -l" -ldflags -w
 abbr -a gob go build
-abbr -a got go test -gcflags=all=-l -v -cover
+abbr -a got go test -gcflags=all=-l -v -cover -tags=intest
 abbr -a gog go get
 abbr -a gom go mod
 abbr -a goc go clean
